2797287b134b44868db760662c9d8c8b26945d54
JB <jbpiot@gmail.com>
2014-02-19 16:35:28 +0100

    Update widget.js to new template

--- widget.js
+++ widget.js
@@ -1,50 +1,56 @@
-WAF.define('QR', function() {
-    "use strict";
-    var widget = WAF.require('waf-core/widget');
-    var QR = widget.create('QR');
-     
-    QR.prototype.init = function () {
-    	var qrcode,
-    		width,
-    		height,
-    		size,
-    		params = {};
+WAF.define('QR', ['waf-core/widget'], function (widget) {
 
-        width = $(this.node).width();
-    	height = $(this.node).height();
-    	size = width;
-    	if (width > height) {
-	 		size = height;
-	 	}
-    	
-    	params.width = size;
-    	params.height = size;
-    	
-    	params.colorDark = this.options.colordark ? this.options.colordark : "#000000";
-    	params.colorLight = this.options.colorlight ? this.options.colorlight : "#ffffff";
-    	params.CorrectLevel = this.options.correctlevel ? this.options.correctlevel : "H";
-    	
-        $(this.node).empty();
-    	this._QR = new QRCode(this.node, params);
-    	
-    	this.content.onChange(function (value) {this.repaint();});
-        
-        this.repaint();
-    };
-     
-   
-     /**
-     * This method recreates the code and fire the event QREvent
-     * @class QR
-     * @method repaint 
-     * 
-      */
+    var QR = widget.create('QR', {
+        init: function () {
+            var qrcode,
+                width,
+                height,
+                size,
+                params = {};
+
+            width = $(this.node).width();
+            height = $(this.node).height();
+            size = width;
+            if (width > height) {
+                size = height;
+            }
+
+            params.width = size;
+            params.height = size;
+
+            params.colorDark = this.colordark();
+            params.colorLight = this.colorlight();
+            params.CorrectLevel = this.correctlevel();
+
+            $(this.node).empty();
+            this._QR = new QRCode(this.node, params);
+
+            this.content.onChange(function (value) {
+                this.repaint();
+            });
+
+            this.repaint();
+        },
+        'content': widget.property({
+            defaultValue: ''
+        }),
+        'colordark': widget.property({
+            defaultValue: '#000000',
+            bindable: false
+        }),
+        'colorlight': widget.property({
+            defaultValue: '#ffffff',
+            bindable: false
+        }),
+        'correctlevel': widget.property({
+            defaultValue: 'H',
+            bindable: false
+        })
+    });
+
     QR.prototype.repaint = function (val) {
-        this._QR.makeCode((this.content())?this.content():'');
+        this._QR.makeCode((this.content()) ? this.content() : '');
     };
-    
-    // adding the content property and defining the set method called for the property
-    QR.addProperty('content', {defaultValue: ''});
-    
+
     return QR;
 });
